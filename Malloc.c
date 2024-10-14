9c1dc7eff18f63c985f69aa4ff5ea9ddf891ca2f
R <93936601+rodrigocbsa@users.noreply.github.com>
2024-10-14 16:51:16 -0300

    Melhoria da didática

--- Malloc.c
+++ Malloc.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* Leia como um livro. */
+
+
 int main(){
 	int *y;
 	
-	//y = malloc(sizeof(int));
+	y = malloc(sizeof(int));
 	
-	*y = 10; // O compilador não apresentará erros. Pois não há erros sintáticos. Isso é possível.
+	*y = 10; // O compilador não apresentará erros. Pois isso é possível de ser feito. Mas está errado considerando o contexto.
+	printf("y = %d", *y); // Não vai funcionar... pois para y não foi separado um espaço na memória de modo que ela consiga receber um valor DIRETAMENTE que será apontado pelo ponteiro. O programa chegará aqui e encerrará sem continuar. Faça o teste!!
+	// Portanto, podemos concluir que, com a função de memory allocation (malloc), ou de alocação de memória, em português, conseguimos atribuir um valor diretamente a uma variável com ponteiro sem utilizar endereços de outras variáveis... ou seja... o malloc estabelecerá o seu endereço. Observe a seguir como a atribuição poderia ser feita sem malloc:
 	
-	printf("y = %d",*y); // Mas não vai funcionar, pois para y não foi separado um espaço na memória de modo que consiga receber um VALOR diretamente apontado pelo ponteiro. O programa chegará aqui e encerrará sem continuar.
+
 	
-	// Portanto, podemos concluir que a função de memory allocation (malloc), ou alocação de memória em português, serve para atribuirmos um valor diretamente a um ponteiro, sem utilizar endereços de memória de outras variáveis.
-	
-	
-	/* Agora, DESCOMENTE a linha 7 e execute o programa! */
 	int x = 10;
-	y = &x;
-	*y = 20; // Isso sim funciona sem o malloc, pois na verdade y é um endereço.
-	printf("\ny = %d = x = %d",*y,x); // E, portanto, *y é a derreferência, que recebe 20, por sua vez, alterando x.
-	// Perceba que tanto o valor 10 quanto o valor 20 só existem em um ÚNICO lugar na memória do computador: em x.
+	int *z = &x;
+	*z = 20; // Isso sim funciona sem o malloc, pois na verdade "z" é um endereço...
+	printf("\ny = %d = x = %d",*z,x); // portanto, "*z" é a derreferência, que recebeu 20 e que, por sua vez, alterou "x". Sacou?
+	// Perceba, então, que tanto o valor 10 quanto o valor 20 só existiram em UM ÚNICO lugar na memória do computador: em "x".
 	
 	
-	
-	// CUIDADO: perceba que na linha 18, quem recebe o endereço é y (E NÃO PONTEIRO DE y!!!). Caso necessário, leia o arquivo Ponteiros.c para entender como ponteiros funcionam. Em resumo: quem recebe o valor do endereço é sempre a variável, sendo que o ponteiro apenas derreferencia ela.
+	// CUIDADO: quem recebe o valor do endereço é sempre a variável, sendo que o ponteiro apenas derreferencia ela. Estude em Ponteiros.c
 }
